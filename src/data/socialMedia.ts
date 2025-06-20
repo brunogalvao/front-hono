079899b520dc2c493a84261e4fcaa2952034d218
Bruno Galvão <bruno_galvao@outlook.com>
2025-06-20 17:10:14 -0300

    :up: add link socialMedia

--- src/data/socialMedia.ts
+++ src/data/socialMedia.ts
@@ -3,17 +3,17 @@ import { FaLinkedin, FaSquareGithub, FaSquareXTwitter } from "react-icons/fa6";
 export const mediaSocial = [
   {
     icon: FaSquareXTwitter,
-    link: "https://twitter.com/AiVision",
+    link: "https://x.com/brunogalvao",
     text: "Twitter",
   },
   {
     icon: FaLinkedin,
-    link: "https://twitter.com/AiVision",
+    link: "https://www.linkedin.com/in/brunoogalvao/",
     text: "Linkedin",
   },
   {
     icon: FaSquareGithub,
-    link: "https://twitter.com/AiVision",
+    link: "https://github.com/brunogalvao",
     text: "Github",
   },
 ];
