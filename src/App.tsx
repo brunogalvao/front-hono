ed86c8f2daddb2a35f293c57e8789e953b869f27
Bruno Galvão <bruno_galvao@outlook.com>
2025-06-20 08:55:56 -0300

    return app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
-import { RouterProvider } from 'react-router-dom';
-import router from '@/routes';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Admin from '@/pages/Admin';
+import List from '@/pages/List';
+import Login from '@/pages/Login';
+import Home from '@/pages/Home';
+import Income from './pages/Income';
+import RegisterUser from '@/components/RegisterUser';
+import EditUser from './pages/EditUser';
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<RegisterUser />} />
+
+        <Route path="/admin" element={<Admin />}>
+          <Route index element={<List />} /> {/* rota padrão */}
+          <Route path="editUser" element={<EditUser />} />
+          <Route path="list" element={<List />} />
+          <Route path="income" element={<Income />} />
+        </Route>
+        <Route path="*" element={<div>404</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
