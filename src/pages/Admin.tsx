3a1ea39e5c7927a54740c25ba26c44280abb6a1a
Bruno Galvão <bruno_galvao@outlook.com>
2025-06-20 08:52:01 -0300

    fix routes

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -112,8 +112,10 @@ function Admin() {
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min px-8 py-6">
-            <Outlet />
+          <div className="min-h-0 flex-1 rounded-xl bg-muted/50 md:min-h-min px-8 py-6 overflow-auto">
+            <div className="h-full w-full">
+              <Outlet />
+            </div>
           </div>
         </div>
       </SidebarInset>
