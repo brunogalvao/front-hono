9bf4ebb1642560372c7dda4defb6263a61b5969a
brunogalvao <bruno_galvao@outlook.com>
2026-05-06 22:27:15 -0300

    :up: ajuste theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from '@tanstack/react-router';
-import { ThemeProvider } from '@/components/theme-provider';
 import { UserProvider } from '@/context/UserContext';
 import { Toaster } from '@/components/ui/sonner';
 import { queryClient } from '@/lib/query-client';
@@ -9,12 +8,10 @@ import { router } from '@/routes/tanstack-router';
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <UserProvider>
-          <RouterProvider router={router} />
-          <Toaster />
-        </UserProvider>
-      </ThemeProvider>
+      <UserProvider>
+        <RouterProvider router={router} />
+        <Toaster />
+      </UserProvider>
     </QueryClientProvider>
   );
 }
